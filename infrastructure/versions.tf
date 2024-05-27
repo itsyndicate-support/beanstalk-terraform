5416bab3ef0fba091ab2f6f715cbe639cb98cc40
DmytroMigirov <dmytromigirov@gmail.com>
2024-05-27 16:08:47 +0200

    Fix infra

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,4 +1,10 @@
 
 terraform {
   required_version = ">= 0.12"
-}
+  backend "s3" {
+    bucket = "state-bucket-512"
+    encrypt = true
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}}
